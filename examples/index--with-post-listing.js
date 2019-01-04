fe472edcd3f4955da09839611bca978f0f9ed050
Melissa Miller <miller.2676@osu.edu>
2019-01-04 09:12:01 -0800

    create examples directory for future feature development

---
+++ examples/index--with-post-listing.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/layout/layout'
+import SEO from '../components/seo/seo'
+
+const IndexPage = ({data}) => (
+  <Layout noHeader>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div className="post-list">
+      <h2>Post Index</h2>
+      <ul>
+        {data.allMarkdownRemark.edges.map(post => (
+          <li>
+            <Link
+              key={post.node.id}
+              to={post.node.frontmatter.path}>
+              {post.node.frontmatter.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </Layout>
+)
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      limit: 5
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+            published
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
