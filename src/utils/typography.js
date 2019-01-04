4428e217bb0b8e8831fc0a4a416bcce2970e2407
Melissa Miller <miller.2676@osu.edu>
2019-01-03 18:36:30 -0800

    create custom typography object instead of using a theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,21 @@
 import Typography from 'typography'
-import noriegaTheme from 'typography-theme-noriega'
-noriegaTheme.baseFontSize = '16px'
-noriegaTheme.scaleRatio = 2
-noriegaTheme.blockMarginBottom = 0.9
-noriegaTheme.baseLineHeight = 1.5
 
-const typography = new Typography(noriegaTheme)
+const typography = new Typography({
+  baseFontSize: '17px',
+  baseLineHeight: 1.55,
+  headerFontFamily: ['Source Sans Pro', 'sans-serif'],
+  bodyFontFamily: ['Source Sans Pro', 'sans-serif'],
+  bodyWeight: 400,
+  headerWeight: 700,
+  boldWeight: 600,
+  googleFonts: [
+    {
+      name: 'Source Sans Pro',
+      styles: ['400','400i','600','700'],
+    },
+  ],
+  scaleRatio: 2.1,
+  blockMarginBottom: 0.8,
+})
 
 export default typography
