e45c230ef4c7bd39e871510976cbd009deca2158
Melissa Miller <miller.2676@osu.edu>
2019-01-22 08:29:48 -0800

    update header to match

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import SocialBlock from './SocialBlock'
 const Wrapper = styled.footer`
   background: ${props => props.theme.blue};
   color: white;
-  border-top: 4px solid ${props => props.theme.blueLight};
+  border-top: 3px solid ${props => props.theme.blueLight};
   padding-top: 20px;
 `
 const FinePrint = styled.p`
