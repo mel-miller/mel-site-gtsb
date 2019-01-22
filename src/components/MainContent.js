b14340ec30e30e16d572d91dfa2a4195e79384e2
Melissa Miller <miller.2676@osu.edu>
2019-01-22 08:24:52 -0800

    add and style sticky footer

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
   padding: 3em 0;
+  flex: 1;
 `
 
 const MainContent = ({content}) => (
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
 
 import Header from '../Header'
 import MainContent from '../MainContent'
+import Footer from '../Footer'
 
 //import global styles here only
 import './global.scss'
 
+//site wrapper
+const SiteWrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`
+
 // Extract Sass variables into a JS object
 const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./vars.scss');
 
-const Layout = ({ children, noHeader }) => (
+const Layout = ({ children, noHeader, noFooter }) => (
   <ThemeProvider theme={theme}>
     <StaticQuery
       query={graphql`
@@ -25,13 +34,17 @@ const Layout = ({ children, noHeader }) => (
         }
       `}
       render={data => (
-        <div>
+        <SiteWrapper>
           { noHeader
             ? null
             : <Header siteTitle={data.site.siteMetadata.title} />
           }
           <MainContent content={children} />
-        </div>
+          { noFooter
+            ? null
+            : <Footer />
+          }
+        </SiteWrapper>
       )}
     />
   </ThemeProvider>
