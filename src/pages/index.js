6526a376e6d67752692e021396dc2ca77f54bdd7
Melissa Miller <miller.2676@osu.edu>
2019-01-21 16:04:04 -0800

    move intro text styles to a styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout/layout'
 import SEO from '../components/SEO'
 import SocialBlock from '../components/SocialBlock'
 import MelMug from '../components/images/MelMug'
 
+const IntroText = styled.p`
+  font-size: 130%;
+  width: 60%;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 600px) {
+    width: 80%;
+  }
+`
+
 const IndexPage = () => (
   <Layout noHeader>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`, `drupal developer`, `designer`]} bodyClass="blue center" />
     <MelMug />
     <h1>Hey there.</h1>
-    <p className="intro-text">My name is Melissa Miller, and I’m a web developer and designer living in San Francisco.</p>
+    <IntroText>My name is Melissa Miller, and I’m a web developer and designer living in San Francisco.</IntroText>
     <SocialBlock />
   </Layout>
 )
