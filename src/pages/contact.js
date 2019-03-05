f0b09cee975e0baea4916a95bb1368e794d58157
M Miller <miller.2676@osu.edu>
2019-03-04 16:23:56 -0800

    test: remove honeypot

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -73,9 +73,7 @@ const ContactPage = () => (
       method="POST"
       action="/success"
       data-netlify="true"
-      data-netlify-honeypot="bot-field"
     >
-    <input type="hidden" name="bot-field" />
       <FormFieldShort>
         <Label for="name">Name</Label>
         <Input name="name" type="text"/>
